ac15434656605679b997429d6369a422b9c059eb
Antoine LEBOISSELIER <contact@aleboisselier.fr>
2016-03-07 09:05:44 +0100

    :boom: Initial Commit :boom:

---
+++ src/com/qmp/rest/services/RestBase.java
@@ -0,0 +1,19 @@
+package com.qmp.rest.services;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
+import net.ko.framework.KoHttp;
+
+public class RestBase {
+
+	@Context
+	protected ServletContext context;
+
+	@Context
+	public void setServletContext(ServletContext context) {
+		this.context = context;
+		KoHttp.kstart(context);
+	}
+
+}
