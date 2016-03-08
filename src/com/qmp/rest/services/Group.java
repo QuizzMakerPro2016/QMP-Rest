1d997f227bc98175856047213f453a6799a59d45
Luctum <hugo.palla@gmail.com>
2016-03-08 12:03:53 +0100

    Basics "get" request for groups and users

--- src/com/qmp/rest/services/Group.java
+++ src/com/qmp/rest/services/Group.java
@@ -6,21 +6,35 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.qmp.rest.models.KGroupe;
+
+import net.ko.framework.KoHttp;
+import net.ko.kobject.KListObject;
 
 @Path("/group")
 public class Group extends RestBase {
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/all")
 	public String all() {
-		return null;
+		KListObject<KGroupe> groups = KoHttp.getDao(KGroupe.class).readAll();
+		return new Gson().toJson(groups.asAL());
 	}
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{id}")
-	public String getOne() {
-		return null;
-
+	public String getOne(@PathParam("id") int id) {
+		KGroupe group = KoHttp.getDao(KGroupe.class).readById(id);
+		if (!group.isLoaded())
+			return "null";
+		return new Gson().toJson(group);
 	}
 
 	@GET
--- src/com/qmp/rest/services/User.java
+++ src/com/qmp/rest/services/User.java
@@ -6,29 +6,45 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.qmp.rest.models.KUtilisateur;
+
+import net.ko.framework.KoHttp;
+import net.ko.kobject.KListObject;
 
 @Path("/user")
 public class User extends RestBase {
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/all")
 	public String all() {
-		return null;
-		/* Todo */
+		KListObject<KUtilisateur> users = KoHttp.getDao(KUtilisateur.class).readAll();
+		return new Gson().toJson(users.asAL());
 	}
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{id}")
-	public String getOne() {
-		return null;
-		/* Todo */
+	public String getOne(@PathParam("id") int id) {
+		KUtilisateur user = KoHttp.getDao(KUtilisateur.class).readById(id);
+		if (!user.isLoaded())
+			return "null";
+		return new Gson().toJson(user);
 	}
 
 	@GET
 	@Path("/{id}/quizzes")
-	public String quizzes() {
+	public String quizzes(@PathParam("id") int id) {
+		/*KGroupe_questionnaire quizzes = KoHttp.getDao(KGroupe_questionnaire.class).readAll("utilisateur_id like '"+id+"'")
+		if (!quizzes.isLoaded())
+			return "null";
+		return new Gson().toJson(quizzes);*/
 		return null;
-		/* Todo */
 	}
 
 	@GET
