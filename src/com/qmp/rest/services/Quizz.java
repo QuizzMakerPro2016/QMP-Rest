6db8b2da153915343e8a9febb32ff1342a699e73
NiBrossault <ni.brossault@laposte.net>
2016-03-14 09:40:46 +0100

    change function in quizz and question

--- src/com/qmp/rest/services/Quizz.java
+++ src/com/qmp/rest/services/Quizz.java
@@ -22,76 +22,10 @@ import net.ko.kobject.KListObject;
  * Quizz REST Functions
  */
 @Path("/quizz")
-public class Quizz extends RestBase {
-	/**
-	 * Index Function
-	 * @return All Quizzes Object in DB
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/")
-	public String all() {
-		return getAll();
+public class Quizz extends CrudRestBase {
+	public Quizz() {
+		super();
+		kobjectClass = KQuestionnaire.class;
+		displayName = "questionnaire";
 	}
-	
-	/**
-	 * Get all Quizzes Object in DB
-	 * @return All Quizzes Object in DB
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/all")
-	public String getAll() {
-		KListObject<KQuestionnaire> quizzes = KoHttp.getDao(KQuestionnaire.class).readAll();
-		return gson.toJson(quizzes.asAL());
-	}
-	
-	/**
-	 * Search a quizz with given ID
-	 * @param id of quizz to get
-	 * @return Quizz JSON Object or NULL
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{id}")
-	public String getOne(@PathParam("id") int id){
-		KQuestionnaire quizz = KoHttp.getDao(KQuestionnaire.class).readById(id);
-		if (!quizz.isLoaded())
-			return "null";
-		return gson.toJson(quizz);
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{id}/user")
-	public String getzazaz(@PathParam("id") int id){
-		KQuestionnaire quizz = KoHttp.getDao(KQuestionnaire.class).readById(id);
-		
-		if (!quizz.isLoaded())
-			return "null";
-		return gson.toJson(quizz.getUtilisateur());
-	}
-	
-	/**
-	 * Add a quizz in DB using form passed in POST Request
-	 * @param formParams POST form with quizz data
-	 * @return Error or Success Message
-	 * @throws SQLException
-	 */
-	/*@PUT
-	@Path("/")
-	@Consumes("application/x-www-form-urlencoded")
-	public String addOne(MultivaluedMap<String, String> formParams)
-			throws SQLException {
-		KQuestionnaire quizzes = new KQuestionnaire();
-
-		String message = "{\"message\": \"Insert OK\"}";
-		
-		String error = setValuesToKObject(quizzes, formParams);
-		if(error != null)
-			return error;
-		
-		KoHttp.getDao(KQuestionnaire.class).create(quizzes);
-		return message;
-	}*/
 }
